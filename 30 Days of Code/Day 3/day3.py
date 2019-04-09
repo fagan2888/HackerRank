726672d54b9c9a24ac5a848b5798437861acbd25
Rohit Midha <rohit.midha23@gmail.com>
2019-04-09 06:19:00 +0530

    Day 3

---
+++ 30 Days of Code/Day 3/day3.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+    if(N % 2 != 0):
+        print("Weird")
+    else:
+        if(N<=5 and N>=2):
+            print("Not Weird")
+        if(N<=20 and N>=6):
+            print("Weird")
+        if(N>20):
+            print("Not Weird")
