6e30022b86f208a37a107ec6316ae048ba166b21
Rohit Midha <rohit.midha23@gmail.com>
2019-04-09 07:26:36 +0530

    Day 20

---
+++ 30 Days of Code/Day20/day20.py
@@ -0,0 +1,20 @@
+#!/bin/python2
+
+import sys
+
+n = int(raw_input().strip())
+a = map(int, raw_input().strip().split(' '))
+# Write Your Code Here
+
+def bubble(arr):
+    count_swap = 0
+    for num in range(len(arr)-1, 0, -1):
+        for i in range(num):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                count_swap +=1
+    return count_swap
+
+print "Array is sorted in {} swaps.".format(bubble(a))
+print "First Element: {}".format(a[0])
+print "Last Element: {}".format(a[-1])
